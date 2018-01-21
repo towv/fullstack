8de677d6e2845df9902e809ce406fe741d622566
Tommi Viiala <twviiala@gmail.com>
2018-01-21 22:50:52 +0200

    t1.10-1.11

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -4,21 +4,26 @@ import ReactDOM from 'react-dom';
 const Statistics = (props) => {
     const state = props.state
     return (
-        <div>
-            <Statistic text={"hyvä"} value={state.good} />
-            <Statistic text={"neutraali"} value={state.neutral} />
-            <Statistic text={"huono"} value={state.bad} />
-            <Statistic text={"keskiarvo"} value={Keskiarvo(state)} />
-            <Statistic text={"positiivisia"} value={Positiivisia(state)} />
-            {/* <Keskiarvo tila={state} />
+        <table>
+            <tbody>
+                <Statistic text={"hyvä"} value={state.good} />
+                <Statistic text={"neutraali"} value={state.neutral} />
+                <Statistic text={"huono"} value={state.bad} />
+                <Statistic text={"keskiarvo"} value={Keskiarvo(state)} />
+                <Statistic text={"positiivisia"} value={Positiivisia(state)} />
+                {/* <Keskiarvo tila={state} />
             <Positiivisia tila={state} /> */}
-        </div>
+            </tbody>
+        </table>
     )
 }
 
 const Statistic = (props) => {
     return (
-        <p>{props.text} {props.value}</p>
+        <tr>
+            <td>{props.text}</td>
+            <td>{props.value}</td>
+        </tr>
     )
 }
 
@@ -85,77 +90,122 @@ class App extends React.Component {
         })
     }
 
+    click = (props) => {
+        const taulu = ['good', 'neutral', 'bad']
+        return () => {
+            this.setState({ [taulu[props]]: this.state[taulu[props]] + 1 })
+        }
+        // const nykyarvo = this.state[props.tila]
+        // if (props === 0) {
+        //     return () => {
+        //         this.setState({
+        //             good: this.state.good + 1
+        //         })
+        //     }
+        // }
+        // if (props == 1) {
+        //     return () => {
+        //         this.setState({
+        //             neutral: this.state.neutral + 1
+        //         })
+        //     }
+        // }
+        // if (props == 2) {
+        //     return () => {
+        //         this.setState({
+        //             bad: this.state.bad + 1
+        //         })
+        //     }
+        // }
+
+        // this.state[props.tila] = nykyarvo + 1
+    }
+
     render() {
         const otsikko1 = 'anna palautetta'
         const otsikko2 = 'statistiikka'
         const state = ["hyvä", "neutraali", "huono"]
+        const kolme = 3
 
-        const Button = (props) => {
-            if (props.onclick === "hyvä") {
-                return (
-                    <button onClick={this.clickGood}>hyvä</button>
-                )
-            }
-            if (props.onclick === "neutraali") {
-                return (
-                    <button onClick={this.clickNeutral}>neutraali</button>
-                )
-            }
-            if (props.onclick === "huono") {
-                return (
-                    <button onClick={this.clickBad}>huono</button>
-                )
-            }
 
+        const Nappi = (props) => {
+            return (
+                <button onClick={this.click(props.fiilis)}>{state[props.fiilis]}</button>
+            )
         }
+
+        // const Button = (props) => {
+        //     if (props.onclick === "hyvä") {
+        //         return (
+        //             <button onClick={this.clickGood}>hyvä</button>
+        //         )
+        //     }
+        //     if (props.onclick === "neutraali") {
+        //         return (
+        //             <button onClick={this.clickNeutral}>neutraali</button>
+        //         )
+        //     }
+        //     if (props.onclick === "huono") {
+        //         return (
+        //             <button onClick={this.clickBad}>huono</button>
+        //         )
+        //     }
+
+        // }
         if (this.state.good + this.state.neutral + this.state.bad === 0) {
             return (
                 <div>
-                <Otsikko otsikko={otsikko1} />
-                <Button onclick={state[0]} />
-                <Button onclick={state[1]} />
-                <Button onclick={state[2]} />
-                <Otsikko otsikko={otsikko2} />
-                <p>ei yhtään palautetta annettu</p>
+                    <Otsikko otsikko={otsikko1} />
+                    <Nappi fiilis={0} />
+                    <Nappi fiilis={1} />
+                    <Nappi fiilis={2} />
+                    {/* <Button onclick={state[0]} />
+                    <Button onclick={state[1]} />
+                    <Button onclick={state[2]} /> */}
+                    <Otsikko otsikko={otsikko2} />
+                    <p>ei yhtään palautetta annettu</p>
                 </div>
             )
         } else {
             return (
                 <div>
-                <Otsikko otsikko={otsikko1} />
-                <Button onclick={state[0]} />
-                <Button onclick={state[1]} />
-                <Button onclick={state[2]} />
-                <Otsikko otsikko={otsikko2} />
-                <Statistics state={this.state} />
+                    <Otsikko otsikko={otsikko1} />
+                    <Nappi fiilis={0} />
+                    <Nappi fiilis={1} />
+                    <Nappi fiilis={2} />
+                    {/* <Button onclick={state[0]} />
+                    <Button onclick={state[1]} />
+                    <Button onclick={state[2]} /> */}
+                    <Otsikko otsikko={otsikko2} />
+                    <Statistics state={this.state} />
                 </div>
             )
         }
-        return (
+        // return (
 
-            <div>
-                <Otsikko otsikko={otsikko1} />
-                <Button onclick={state[0]} />
-                <Button onclick={state[1]} />
-                <Button onclick={state[2]} />
-                {/* <button onClick={this.clickGood}>hyvä</button>
-                    <button onClick={this.clickNeutral}>neutraali</button>
-                    <button onClick={this.clickBad}>huono</button> */}
-                <Otsikko otsikko={otsikko2} />
+        //     <div>
+        //         <Otsikko otsikko={otsikko1} />
+        //         <Button onclick={state[0]} />
+        //         <Button onclick={state[1]} />
+        //         <Button onclick={state[2]} />
+        //         {/* <button onClick={this.clickGood}>hyvä</button>
+        //             <button onClick={this.clickNeutral}>neutraali</button>
+        //             <button onClick={this.clickBad}>huono</button> */}
+        //         <Otsikko otsikko={otsikko2} />
 
-                if (this.state.good + this.state.neutral + this.state.bad === 0) {
-                    <p>ei yhtään palautetta annettu</p>
-                } else {
-                    <Statistics state={this.state} />
-                }
+        //         if (this.state.good + this.state.neutral + this.state.bad === 0) {
+        //             <p>ei yhtään palautetta annettu</p>
+        //         } else {
+        //             <Statistics state={this.state} />
+        //         }
 
-                {/* <p>hyvä {this.state.good}</p>
-                    <p>neutraali {this.state.neutral}</p>
-                    <p>huono {this.state.bad}</p>
-                    <Keskiarvo tila={this.state} />
-                    <Positiivisia tila={this.state} /> */}
-            </div>
-        )
+        //         {/* <p>hyvä {this.state.good}</p>
+        //             <p>neutraali {this.state.neutral}</p>
+        //             <p>huono {this.state.bad}</p>
+        //             <Keskiarvo tila={this.state} />
+        //             <Positiivisia tila={this.state} /> */}
+        //     </div>
+        // )
     }
 }
 ReactDOM.render(
